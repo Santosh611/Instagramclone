69c26f3362f94f0980bebda47e74d17c6d70c150
Santosh <santoshalamandha2000@gmail.com>
2021-07-13 15:04:39 +0530

    Final Home page edit !!

--- lib/insta_list.dart
+++ lib/insta_list.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:instagramclone/insta_stories.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class InstaList extends StatelessWidget {
+import 'insta_stories.dart';
+
+class InstaList extends StatefulWidget {
+  @override
+  _InstaListState createState() => _InstaListState();
+}
+
+class _InstaListState extends State<InstaList> {
+  bool isPressed = false;
+
   @override
   Widget build(BuildContext context) {
     var deviceSize = MediaQuery.of(context).size;
     return ListView.builder(
       itemCount: 5,
-      itemBuilder: (content, index) => index == 0
+      itemBuilder: (context, index) => index == 0
           ? new SizedBox(
               child: new InstaStories(),
               height: deviceSize.height * 0.11,
@@ -17,27 +26,123 @@ class InstaList extends StatelessWidget {
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Padding(
-                        padding:
-                            const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0)),
-                    new Container(
-                      decoration: new BoxDecoration(
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          new Container(
+                            height: 40.0,
+                            width: 40.0,
+                            decoration: new BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: new DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image: new NetworkImage(
+                                      "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg")),
+                            ),
+                          ),
+                          new SizedBox(
+                            width: 10.0,
+                          ),
+                          new Text(
+                            "imthpk",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      new IconButton(
+                        icon: Icon(Icons.more_vert),
+                        onPressed: null,
+                      )
+                    ],
+                  ),
+                ),
+                Flexible(
+                  fit: FlexFit.loose,
+                  child: new Image.network(
+                    "https://images.pexels.com/photos/672657/pexels-photo-672657.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          new IconButton(
+                            icon: new Icon(isPressed
+                                ? Icons.favorite
+                                : FontAwesomeIcons.heart),
+                            color: isPressed ? Colors.red : Colors.black,
+                            onPressed: () {
+                              setState(() {
+                                isPressed = !isPressed;
+                              });
+                            },
+                          ),
+                          new SizedBox(
+                            width: 16.0,
+                          ),
+                          new Icon(
+                            FontAwesomeIcons.comment,
+                          ),
+                          new SizedBox(
+                            width: 16.0,
+                          ),
+                          new Icon(FontAwesomeIcons.paperPlane),
+                        ],
+                      ),
+                      new Icon(FontAwesomeIcons.bookmark)
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    "Liked by Santosh A M , SA and 528,331 others",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      new Container(
+                        height: 40.0,
+                        width: 40.0,
+                        decoration: new BoxDecoration(
                           shape: BoxShape.circle,
                           image: new DecorationImage(
                               fit: BoxFit.fill,
                               image: new NetworkImage(
-                                  "https://www.cheatsheet.com/wp-content/uploads/2020/11/Marvel-star-Robert-Downey-Jr-1024x692.jpg"))),
-                    ),
-                    new SizedBox(
-                      width: 10.0,  
-                    ),
-                    new Text(
-                      "RDJ",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    )
-                  ],
+                                  "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg")),
+                        ),
+                      ),
+                      new SizedBox(
+                        width: 10.0,
+                      ),
+                      Expanded(
+                        child: new TextField(
+                          decoration: new InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Add a comment...",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child:
+                      Text("1 Day Ago", style: TextStyle(color: Colors.grey)),
                 )
               ],
             ),
