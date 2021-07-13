81d9b79cc9739a595751da7272531ffa8ee7c9d1
Santosh <santoshalamandha2000@gmail.com>
2021-07-13 11:36:52 +0530

    List of post

---
+++ lib/insta_list.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:instagramclone/insta_stories.dart';
+
+class InstaList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var deviceSize = MediaQuery.of(context).size;
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (content, index) => index == 0
+          ? new SizedBox(
+              child: new InstaStories(),
+              height: deviceSize.height * 0.11,
+            )
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0)),
+                    new Container(
+                      decoration: new BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: new DecorationImage(
+                              fit: BoxFit.fill,
+                              image: new NetworkImage(
+                                  "https://www.cheatsheet.com/wp-content/uploads/2020/11/Marvel-star-Robert-Downey-Jr-1024x692.jpg"))),
+                    ),
+                    new SizedBox(
+                      width: 10.0,
+                    ),
+                    new Text(
+                      "RDJ",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )
+                  ],
+                )
+              ],
+            ),
+    );
+  }
+}
